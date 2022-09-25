579ed01c52f9f04996cf3e005d36189a4bb61f63
Federico Miras <miras.federico@gmail.com>
2022-09-25 15:08:07 -0300

    rewrites middleware

--- main.tsx
+++ main.tsx
@@ -1,6 +1,31 @@
 /** @jsx h */
 
-import blog, { ga, redirects } from 'blog'
+import blog, { ga } from 'blog'
+import { BlogMiddleware, BlogContext } from 'blog/types'
+
+export function rewrites(rewritesMap: Record<string, string>): BlogMiddleware {
+  return async function (req: Request, ctx: BlogContext): Promise<Response> {
+    const { pathname } = new URL(req.url)
+
+    let maybeRewrite = rewritesMap[pathname]
+
+    if (!maybeRewrite) {
+      // trim leading slash
+      maybeRewrite = rewritesMap[pathname.slice(1)]
+    }
+
+    if (maybeRewrite) {
+      return new Response(null, {
+        status: 307,
+        headers: {
+          location: maybeRewrite
+        }
+      })
+    }
+
+    return await ctx.next()
+  }
+}
 
 blog({
   title: "Fefo's Blog",
@@ -9,11 +34,13 @@ blog({
   avatar: 'https://fmiras.com/_next/image?url=%2Fimages%2Fprofile.jpg&w=384&q=75',
   avatarClass: 'rounded-full',
   author: 'Federico Mirás',
-
   middlewares: [
     ga('G-G64GWBMMKR'),
-    redirects({
-      '/hello_world.html': '/hello_world'
+    rewrites({
+      '/pluggy-2021-federico': 'https://pluggy.ai/blog/pluggy-2021-federico',
+      '/decentraland-on-now': 'https://decentraland.org/blog/announcements/decentraland-on-now',
+      '/decentraland-and-wallet-connect':
+        'https://decentraland.org/blog/announcements/decentraland-and-wallet-connect'
     })
   ]
 })
